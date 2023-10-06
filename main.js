5fa6ba7d283ba90108813ebdfad0b2dad746e31d
Arath Astorga <arastorga@outlook.com>
2023-10-05 22:07:10 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.scss'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = /*html*/`
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
